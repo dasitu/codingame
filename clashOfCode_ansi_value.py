52bb608501d63e55998c39ca35a31e76a9b2a6c3
dasitu <dasitu_h@163.com>
2018-12-11 11:07:59 +0800

    clean the code

--- clashOfCode_ansi_value.py
+++ clashOfCode_ansi_value.py
@@ -1,26 +1,14 @@
 import sys
-import math
 import re
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 meme_str = input()
-
+#meme_str = 'ha cse'
 meme_strs = re.sub(r'\W+', '', meme_str)
 meme_strs = "".join(set(meme_strs))
 sum_value = 0
+output = 0
 for c in meme_strs:
     sum_value += ord(c)
-
-output = sum_value % len(meme_str)
-
-if output > 10:
-    output = 10
-
+output = 10 if output > 10 else sum_value % len(meme_str)
 print(meme_strs, file=sys.stderr)
-
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
-
 print(str(output)+'/10')
