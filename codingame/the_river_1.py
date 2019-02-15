0de9422bdb356b53fe9cdbe627d8acf00f79dbb0
dasitu <dasitu_h@163.com>
2019-02-15 15:12:31 +0800

    Completed the river1 with simpler solution

--- codingame/the_river_1.py
+++ codingame/the_river_1.py
@@ -1,24 +1,22 @@
 import sys
-import math
 
 
-def cal_next(r: list) -> list:
-    last = r[-1]
-    for d in str(last):
-        last += int(d)
-    r.append(last)
+def cal_next(r: int) -> int:
+    for d in str(r):
+        r += int(d)
     return r
 
 
-r_1 = [int(input())]
-r_2 = [int(input())]
-
-while r_1[-1] != r_2[-1]:
-    if r_1[-1] > r_2[-1]:
+# r_1 = int(input())
+# r_2 = int(input())
+r_1 = 32
+r_2 = 47
+while r_1 != r_2:
+    if r_1 > r_2:
         r_2 = cal_next(r_2)
     else:
         r_1 = cal_next(r_1)
-    # print("r1:{}".format(r_1), file=sys.stderr)
-    # print("r2:{}".format(r_2), file=sys.stderr)
+    print("r1:{}".format(r_1), file=sys.stderr)
+    print("r2:{}".format(r_2), file=sys.stderr)
 
-print(r_1[-1])
+print(r_1)
