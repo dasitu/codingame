ec9b36729a69f9b3c6d63c21d726aa0ac6141c8b
dasitu <dasitu_h@163.com>
2019-07-03 14:42:51 +0800

    Complete Dart101

--- codingame-java/src/main/java/easy/TheDart101.java
+++ codingame-java/src/main/java/easy/TheDart101.java
@@ -1,7 +1,90 @@
+// https://www.codingame.com/ide/puzzle/the-dart-101
+
 package easy;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class TheDart101 {
   public static void main(String[] args) {
-    System.out.println("Hugo");
+    Scanner in = new Scanner(System.in);
+    int playerCount = in.nextInt();
+    String[] players = new String[playerCount];
+    String[] shoots = new String[playerCount];
+    int winnerIndex = 0;
+    int winnerScore = 0;
+    int winnerRound = 0;
+    if (in.hasNextLine()) {
+      in.nextLine();
+    }
+    for (int i = 0; i < playerCount; i++) {
+      players[i] = in.nextLine();
+    }
+    for (int i = 0; i < playerCount; i++) {
+      shoots[i] = in.nextLine();
+      int[] playerScoreAndRound = calculateScore(shoots[i]);
+      int playerScore = playerScoreAndRound[0];
+      int playerRound = playerScoreAndRound[1];
+      if (playerScore > winnerScore) {
+        winnerScore = playerScore;
+        winnerRound = playerRound;
+        winnerIndex = i;
+      } else if (playerScore == winnerScore && playerRound < winnerRound) {
+        winnerRound = playerRound;
+        winnerIndex = i;
+      }
+    }
+
+    System.out.println(players[winnerIndex]);
+  }
+
+  private static int[] calculateScore(String shoots) {
+    String[] shoot = shoots.split(" ");
+    int[] totalScoreAndRound = {0, 0};
+    int shootIndex;
+    int shootIndexInRound;
+    for (shootIndex = 0; shootIndex < shoot.length; shootIndex += shootIndexInRound) {
+      System.err.println("round:" + totalScoreAndRound[1]);
+      int roundScore = 0;
+      StringBuilder missState = new StringBuilder();
+      for (shootIndexInRound = 0;
+           shootIndexInRound < 3 && (shootIndexInRound + shootIndex) < shoot.length;
+           shootIndexInRound++) {
+        String singleShoot = shoot[shootIndex + shootIndexInRound];
+        System.err.println(singleShoot);
+
+        if (singleShoot.contains("*")) { // * situation
+          String[] multipleValue = singleShoot.split(Pattern.quote("*"));
+          roundScore += Integer.parseInt(multipleValue[0]) * Integer.parseInt(multipleValue[1]);
+          missState.append("O");
+        } else if (singleShoot.equals("X")) { // X handling
+          roundScore -= 20;
+          missState.append("X");
+          if (missState.toString().equals("XXX")) {
+            totalScoreAndRound[0] = 0;
+            roundScore = 0;
+          } else if (missState.toString().contains("XX")) {
+            roundScore -= 10;
+          }
+        } else { // just add the number
+          roundScore += Integer.parseInt(singleShoot);
+          missState.append("O");
+        }
+
+        // handle the exceed situation, round is end
+        int total = totalScoreAndRound[0] + roundScore;
+        if (total > 101) {
+          System.err.println("totalScore:" + total + " Exceed 101.");
+          roundScore = 0;
+          shootIndexInRound++;
+          break;
+        }
+      }
+      totalScoreAndRound[1]++;
+      totalScoreAndRound[0] += roundScore;
+      System.err.println("roundScore:" + roundScore);
+      System.err.println("totalScore:" + totalScoreAndRound[0]);
+    }
+    return totalScoreAndRound;
   }
 }
--- codingame-java/src/test/java/easy/TheDart101Test.java
+++ codingame-java/src/test/java/easy/TheDart101Test.java
@@ -6,7 +6,7 @@ import utils.TestUtils;
 public class TheDart101Test extends TestUtils {
 
   @Test
-  public void test2Players(){
+  public void twoPlayers(){
     String[] input = {
             "2",
             "Hugo",
@@ -19,7 +19,7 @@ public class TheDart101Test extends TestUtils {
   }
 
   @Test
-  public void test1Win1Loser(){
+  public void oneWinOneLose(){
     String[] input = {
             "2",
             "Lisa",
@@ -32,7 +32,7 @@ public class TheDart101Test extends TestUtils {
   }
 
   @Test
-  public void testOneMissed(){
+  public void oneMissed(){
     String[] input = {
             "2",
             "Candice",
@@ -45,7 +45,7 @@ public class TheDart101Test extends TestUtils {
   }
 
   @Test
-  public void testTowMissed(){
+  public void twoMissed(){
     String[] input = {
             "2",
             "Fred",
@@ -56,4 +56,73 @@ public class TheDart101Test extends TestUtils {
     String[] output = {"Fred"};
     testWithInputOutput(input, output);
   }
+
+  @Test
+  public void OverTheScore(){
+    String[] input = {
+            "2",
+            "Noemie",
+            "Nicolas",
+            "2 5 5 19 5 6 10 1 20 1 2 5",
+            "3*17 5 12 5 2 3 15 9 20 3",
+    };
+    String[] output = {"Nicolas"};
+    testWithInputOutput(input, output);
+  }
+
+  @Test
+  public void twoPlayersAndLotOfShoots(){
+    String[] input = {
+           "2",
+           "Yoan",
+           "Ludo",
+           "20 1 5 2*5 3*18 X X 19 11 10 12 16 7 2*11 X 3*17 3*7",
+           "3*20 3*19 3*17 25 20 X X X 2*25 3*14 X X X 3*20 X 5 X 3*18",
+    };
+    String[] output = {"Ludo"};
+    testWithInputOutput(input, output);
+  }
+
+  @Test
+  public void fourPlayers(){
+    String[] input = {
+            "4",
+            "Eric",
+            "Delphine",
+            "Patricia",
+            "Yan",
+            "6 2 7 15 2*10 8 2 3 6 15 2 1 3 11",
+            "4 3 2 1 1 1 X X 10 2 3 5",
+            "X X X X X X X X X 2*25 3*17",
+            "3*15 2*10 3*5 2*12 2*7 2*7 3*15",
+    };
+    String[] output = {"Patricia"};
+    testWithInputOutput(input, output);
+  }
+
+  @Test
+  public void eightPlayers(){
+    String[] input = {
+           "8",
+           "Eric",
+           "Delphine",
+           "Patricia",
+           "Yan",
+           "David",
+           "Hugo",
+           "Ludo",
+           "Yoan",
+           "4 3 2 1 1 1 X X 10",
+           "X X X X X X X X X 2*25 3*17",
+           "6 2 7 15 2*10 8 2 3 6 15 2 1 3 11",
+           "3*15 2*10 3*5 2*12 2*7 2*7 3*15",
+           "2*25 3*17",
+           "10 5 3*18 20 X 2*14 4",
+           "5 5 10 2*19 5 6 2*5 1 20 1",
+           "10 5 3*18 20 X 2*14 10",
+    };
+    String[] output = {"David"};
+    testWithInputOutput(input, output);
+  }
+
 }
