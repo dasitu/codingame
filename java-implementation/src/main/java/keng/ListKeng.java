aa087a2cffa01fa8b965defc230277f099f72c59
dasitu <dasitu_h@163.com>
2019-07-10 13:49:36 +0800

    start new package called keng to record some example during learning

---
+++ java-implementation/src/main/java/keng/ListKeng.java
@@ -0,0 +1,87 @@
+package keng;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListKeng {
+  public static void main(String[] args) {
+    List<String> immutableList = Arrays.asList("a", "b", "c", "d", "e", "f");
+    List<String> mutableList = new ArrayList<>(immutableList);
+
+    // ConcurrentModificationException
+//    for (String n: mutableList){
+//      mutableList.remove(n);
+//      System.out.println(mutableList.toString());
+//    }
+
+    // no exception, but not working as expected
+    System.out.println(mutableList);
+    for (int i = 0; i < mutableList.size(); i++) {
+      System.out.println("removing by index:" + i);
+      mutableList.remove(i);
+      System.out.println(mutableList);
+    }
+
+    // reset back to original list
+    System.out.println("##########");
+    mutableList.clear();
+    mutableList.addAll(immutableList);
+    // no exception, but not working as expected
+    System.out.println(mutableList);
+    int originSize = mutableList.size();
+    for (int i = 0; i < originSize; i++) {
+      System.out.println("removing by index:" + i);
+      mutableList.remove(0);
+      System.out.println(mutableList);
+    }
+
+    // reset back to original list
+    System.out.println("##########");
+    mutableList.clear();
+    mutableList.addAll(immutableList);
+
+    // corrected way with for loop, this is universal for all type
+    System.out.println(mutableList);
+    for (Iterator<String> iterator = mutableList.iterator(); iterator.hasNext(); ) {
+      String element = iterator.next();
+      System.out.println("removing:" + element);
+      iterator.remove();
+      System.out.println(mutableList);
+    }
+    ;
+
+    // reset back to original list
+    System.out.println("##########");
+    mutableList.clear();
+    mutableList.addAll(immutableList);
+
+    // correct way with while loop, this is universal for all type
+    System.out.println(mutableList);
+    Iterator<String> iterator = mutableList.iterator();
+    while (iterator.hasNext()) {
+      String element = iterator.next();
+      System.out.println("removing:" + element);
+      iterator.remove();
+      System.out.println(mutableList);
+    }
+
+    // reset back to original list
+    System.out.println("##########");
+    mutableList.clear();
+    mutableList.addAll(immutableList);
+
+    // correct way with ListIterator, this is only for List
+    System.out.println(mutableList);
+    ListIterator<String> listIterator = mutableList.listIterator();
+    while (listIterator.hasNext()) {
+      int index = listIterator.nextIndex();
+      String element = listIterator.next();
+      System.out.printf("removing [%d]: %s \n", index, element);
+      listIterator.remove();
+      System.out.println(mutableList);
+    }
+  }
+}
