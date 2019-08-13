8e7f3617721acfec15e76c9be6bc68972d998de5
dasitu <dasitu_h@163.com>
2019-08-13 11:03:15 +0800

    complete sudoku by recursion

--- java-implementation/src/main/java/codingame/medium/SudokuSolver.java
+++ java-implementation/src/main/java/codingame/medium/SudokuSolver.java
@@ -6,20 +6,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class SudokuSolver {
 
-  private int boardWidth = 9;
-  private int boardHeight = 9;
   private Map<Integer, List<Integer>> candidateMap = new HashMap<>();
   private List<List<Integer>> sudokuBoard;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
 
     List<List<Integer>> puzzle = new ArrayList<>();
     Scanner in = new Scanner(System.in);
@@ -48,7 +49,7 @@ public class SudokuSolver {
     // initial the candidateMap with all possibilities
     // index represent the position in the sudoku table
     // continues index from 0 to 81
-    for (int i = 0; i < boardHeight * boardWidth; i++) {
+    for (int i = 0; i < 9 * 9; i++) {
       candidateMap.put(i, new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
     }
     sudokuBoard = puzzle;
@@ -56,8 +57,121 @@ public class SudokuSolver {
     printBoardAndCandidatesHtmlFile();
   }
 
-  public List<List<Integer>> resolveSudoku() {
+  public List<List<Integer>> resolveSudoku() throws InterruptedException {
+    resolveSudokuByRules();
+    resolveSudokuByRecursion(0, 0, sudokuBoard);
+    return sudokuBoard;
+  }
 
+  private boolean isRowValid(int row, List<List<Integer>> puzzle) {
+    return isUnitValid(puzzle.get(row), puzzle);
+  }
+
+  private boolean isColValid(int col, List<List<Integer>> puzzle) {
+    return isUnitValid(getColValues(col, puzzle), puzzle);
+  }
+
+  private boolean isBlockValid(int row, int col, List<List<Integer>> puzzle) {
+    return isUnitValid(getBlockValues(row, col, puzzle), puzzle);
+  }
+
+  private boolean isUnitValid(List<Integer> unitValues, List<List<Integer>> puzzle) {
+    List<Integer> values = new ArrayList<>(unitValues);
+    values.removeIf(n -> n == 0);
+    Set<Integer> hashSet = new LinkedHashSet<>(values);
+    return hashSet.size() == values.size();
+  }
+
+  boolean isFinalAnswer(List<List<Integer>> puzzle) {
+    // check whether found the final answer
+    List<Integer> expectedList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    // check row
+    int rowMatchedCount = 9;
+    for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
+      List<Integer> rowNums = new ArrayList<>(puzzle.get(rowIndex));
+      Collections.sort(rowNums);
+      if (rowNums.equals(expectedList)) {
+        rowMatchedCount -= 1;
+      }
+    }
+
+    // check col
+    int colMatchedCount = 9;
+    for (int colIndex = 0; colIndex < 9; colIndex++) {
+      List<Integer> colNums = new ArrayList<>(getColValues(colIndex, puzzle));
+      Collections.sort(colNums);
+      if (colNums.equals(expectedList)) {
+        colMatchedCount -= 1;
+      }
+    }
+
+    // check block
+    int blockMatchedCount = 9;
+    for (List<Integer> rowAndCol : getAllBlockTopLeftPos()) {
+      List<Integer> blockNums =
+          new ArrayList<>(getBlockValues(rowAndCol.get(0), rowAndCol.get(1), puzzle));
+      Collections.sort(blockNums);
+      if (blockNums.equals(expectedList)) {
+        blockMatchedCount -= 1;
+      }
+    }
+
+    return (rowMatchedCount + colMatchedCount + blockMatchedCount) == 0;
+  }
+
+  private Boolean resolveSudokuByRecursion(int row, int col, List<List<Integer>> puzzle) throws InterruptedException {
+    //printBoardAndCandidatesHtmlFile("target/classes/sudokuTempBoard.html", puzzle);
+    //TimeUnit.SECONDS.sleep(1);
+    List<List<Integer>> mirrorBoard = new ArrayList<>();
+    for (List<Integer> rowValues: puzzle){
+      List<Integer> mirrorRow = new ArrayList<>(rowValues);
+      mirrorBoard.add(mirrorRow);
+    }
+    if (isFinalAnswer(puzzle)) {
+      sudokuBoard = puzzle;
+      return true;
+    }
+
+    if (puzzle.get(row).get(col) != 0) {
+      List<Integer> next = getNextEmptyPos(row, col, mirrorBoard);
+      if (next != null) {
+        row = next.get(0);
+        col = next.get(1);
+      } else {
+        return true;
+      }
+    }
+
+    // get the next index
+    int nextRow;
+    int nextCol;
+    List<Integer> next = getNextEmptyPos(row, col, mirrorBoard);
+    if (next != null) {
+      nextRow = next.get(0);
+      nextCol = next.get(1);
+    } else {
+      nextRow = row;
+      nextCol = col;
+    }
+
+    List<Integer> candidates = getCandidate(row, col);
+    for (Integer candidate : candidates) {
+      mirrorBoard.get(row).set(col, candidate);
+      if (!isRowValid(row, mirrorBoard)
+          || !isColValid(col, mirrorBoard)
+          || !isBlockValid(row, col, mirrorBoard)) {
+        continue;
+      }
+      boolean found = resolveSudokuByRecursion(nextRow, nextCol, mirrorBoard);
+      if (found) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private void resolveSudokuByRules() {
     // fill with only one candidate
     for (int i = 0; i < 9; i++) {
       updateBoardForSingleCandidate();
@@ -69,33 +183,87 @@ public class SudokuSolver {
       updateBoardForSingleCandidate();
     }
 
+    updateBoardForSinglePossibilityByBlock();
+    updateBoardForSinglePossibilityByRow();
+    updateBoardForSinglePossibilityByCol();
+    refreshCandidateMap();
+
+    for (int i = 0; i < 5; i++) {
+      updateBoardForSingleCandidate();
+    }
+
     printBoardAndCandidatesHtmlFile();
-    return sudokuBoard;
   }
 
+  private void updateBoardForSinglePossibilityByRow() {
+    for (int row = 0; row < 9; row++) {
+      List<Integer> rowCandidateMapIndexes = getRowCandidateMapIndexes(row);
+      updateBoardForSinglePossibility(rowCandidateMapIndexes);
+    }
+  }
+
+  private void updateBoardForSinglePossibilityByCol() {
+    for (int col = 0; col < 9; col++) {
+      List<Integer> colCandidateMapIndexes = getColCandidateMapIndexes(col);
+      updateBoardForSinglePossibility(colCandidateMapIndexes);
+    }
+  }
+
+  private void updateBoardForSinglePossibilityByBlock() {
+    for (List<Integer> pos : getAllBlockTopLeftPos()) {
+      List<Integer> colCandidateMapIndexes = getBlockCandidateMapIndexes(pos.get(0), pos.get(1));
+      updateBoardForSinglePossibility(colCandidateMapIndexes);
+    }
+  }
+
+  private void updateBoardForSinglePossibility(List<Integer> candidateMapIndexes) {
+    for (Integer candidateMapIndex : candidateMapIndexes) {
+      List<Integer> candidates = candidateMap.get(candidateMapIndex);
+      if (candidates == null) {
+        continue;
+      }
+      for (Integer candidate : candidates) {
+        boolean isUnique = true;
+        for (Integer otherCandidateMapIndex : candidateMapIndexes) {
+          if (otherCandidateMapIndex.equals(candidateMapIndex)) {
+            continue;
+          }
+          if (!candidateMap.containsKey(otherCandidateMapIndex)) {
+            continue;
+          }
+          if (candidateMap.get(otherCandidateMapIndex).contains(candidate)) {
+            isUnique = false;
+            break;
+          }
+        }
+        if (isUnique) {
+          setSudokuBoardByCandidateMapIndex(candidateMapIndex, candidate);
+        }
+      }
+    }
+  }
 
   private void updateBoardForSingleCandidate() {
     for (Integer candidateMapIndex : candidateMap.keySet()) {
       List<Integer> candidates = candidateMap.get(candidateMapIndex);
       if (candidates.size() == 1) {
-        int[] pos = calculatePosFromCandidateMapIndex(candidateMapIndex);
-        sudokuBoard.get(pos[0]).set(pos[1], candidates.get(0));
+        setSudokuBoardByCandidateMapIndex(candidateMapIndex, candidates.get(0));
       }
     }
     refreshCandidateMap();
   }
 
   private void updateCandidateMapForPairByCol() {
-    for (int col = 0; col < boardWidth; col++) {
+    for (int col = 0; col < 9; col++) {
       List<Integer> colCandidateMapIndexes = getColCandidateMapIndexes(col);
       updateCandidateMapForPair(colCandidateMapIndexes);
     }
   }
 
   private void updateCandidateMapForPairByRow() {
-    for (int row = 0; row < boardHeight; row++) {
-      List<Integer> colCandidateMapIndexes = getRowCandidateMapIndexes(row);
-      updateCandidateMapForPair(colCandidateMapIndexes);
+    for (int row = 0; row < 9; row++) {
+      List<Integer> rowCandidateMapIndexes = getRowCandidateMapIndexes(row);
+      updateCandidateMapForPair(rowCandidateMapIndexes);
     }
   }
 
@@ -112,9 +280,9 @@ public class SudokuSolver {
           if (commonCandidatesMap.containsKey(candidate)) {
             commonCandidatesMap.get(candidate).add(index);
           } else {
-            commonCandidatesMap.put(candidate, new ArrayList<>(
-                    Arrays.asList(rowOrColCandidateMapIndexes.get(i), index))
-            );
+            commonCandidatesMap.put(
+                candidate,
+                new ArrayList<>(Arrays.asList(rowOrColCandidateMapIndexes.get(i), index)));
           }
         }
       }
@@ -145,7 +313,7 @@ public class SudokuSolver {
               continue;
             }
             if (candidateMap.get(index).contains(common)
-                    && !commonCandidateIndexes.contains(index)) {
+                && !commonCandidateIndexes.contains(index)) {
               candidateMap.get(index).remove(common);
             }
           }
@@ -156,12 +324,15 @@ public class SudokuSolver {
   }
 
   private void printBoardAndCandidatesHtmlFile() {
-
     String htmlFilePath = "target/classes/sudokuBoard.html";
+    printBoardAndCandidatesHtmlFile(htmlFilePath, sudokuBoard);
+  }
 
+  private void printBoardAndCandidatesHtmlFile(String htmlFilePath, List<List<Integer>> board) {
     try {
       FileWriter htmlBoardWriter = new FileWriter(htmlFilePath);
-      String head = "<!DOCTYPE html>\n"
+      String head =
+          "<!DOCTYPE html>\n"
               + "<html lang=\"en\">\n"
               + "<head>\n"
               + "<link rel=\"stylesheet\" type=\"text/css\" href=\"sudokuTable.css\">\n"
@@ -170,9 +341,9 @@ public class SudokuSolver {
               + "<div class=\"main\"><table>";
       String tail = "</div></table>\n</body>\n</html>";
       htmlBoardWriter.write(head);
-      for (int row = 0; row < sudokuBoard.size(); row++) {
+      for (int row = 0; row < board.size(); row++) {
         htmlBoardWriter.write("<tr>");
-        List<Integer> line = sudokuBoard.get(row);
+        List<Integer> line = board.get(row);
         for (int col = 0; col < line.size(); col++) {
           Integer valueInBoard = line.get(col);
           String printContent = String.valueOf(valueInBoard);
@@ -191,7 +362,7 @@ public class SudokuSolver {
   }
 
   private List<Integer> getCandidate(int row, int col) {
-    int candidateIndex = row * boardWidth + col;
+    int candidateIndex = row * 9 + col;
     return candidateMap.get(candidateIndex);
   }
 
@@ -214,9 +385,9 @@ public class SudokuSolver {
     return candidateTable.toString();
   }
 
-  List<Integer> getColValues(int col) {
+  static List<Integer> getColValues(int col, List<List<Integer>> puzzle) {
     List<Integer> colValues = new ArrayList<>();
-    for (List<Integer> line : sudokuBoard) {
+    for (List<Integer> line : puzzle) {
       for (int colIndex = 0; colIndex < line.size(); colIndex++) {
         if (colIndex == col) {
           colValues.add(line.get(colIndex));
@@ -226,13 +397,27 @@ public class SudokuSolver {
     return colValues;
   }
 
+  List<Integer> getColValues(int col) {
+    return getColValues(col, sudokuBoard);
+  }
+
+  static List<List<Integer>> getAllBlockTopLeftPos() {
+    List<List<Integer>> allBlockTopLeftPos = new ArrayList<>();
+    for (int row = 0; row < 9; row += 3) {
+      for (int col = 0; col < 9; col += 3) {
+        allBlockTopLeftPos.add(Arrays.asList(row, col));
+      }
+    }
+    return allBlockTopLeftPos;
+  }
+
   List<Integer> getBlockCandidateMapIndexes(int row, int col) {
     int[] topLeft = calculateBlockFirstPos(row, col);
     int startPosMapIndex = calculateCandidateMapIndex(topLeft[0], topLeft[1]);
     List<Integer> blockCandidateMapIndexes = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        blockCandidateMapIndexes.add(startPosMapIndex + i * boardWidth + j);
+        blockCandidateMapIndexes.add(startPosMapIndex + i * 9 + j);
       }
     }
     return blockCandidateMapIndexes;
@@ -240,8 +425,8 @@ public class SudokuSolver {
 
   private List<Integer> getColCandidateMapIndexes(int col) {
     List<Integer> colCandidateMapIndexes = new ArrayList<>();
-    for (int i = 0; i < boardHeight; i++) {
-      int colIndex = i * boardWidth + col;
+    for (int i = 0; i < 9; i++) {
+      int colIndex = i * 9 + col;
       colCandidateMapIndexes.add(colIndex);
     }
     return colCandidateMapIndexes;
@@ -249,40 +434,78 @@ public class SudokuSolver {
 
   private List<Integer> getRowCandidateMapIndexes(int row) {
     List<Integer> rowCandidateMapIndexes = new ArrayList<>();
-    for (int i = 0; i < boardHeight; i++) {
-      int rowIndex = boardWidth * row + i;
+    for (int i = 0; i < 9; i++) {
+      int rowIndex = 9 * row + i;
       rowCandidateMapIndexes.add(rowIndex);
     }
     return rowCandidateMapIndexes;
   }
 
-  private int[] calculatePosFromCandidateMapIndex(int candidateMapIndex) {
+  private void setSudokuBoardByCandidateMapIndex(int candidateMapIndex, int value) {
+    int[] rowAndCol = calculatePosFromCandidateMapIndex(candidateMapIndex);
+    sudokuBoard.get(rowAndCol[0]).set(rowAndCol[1], value);
+  }
+
+  private static int[] calculatePosFromCandidateMapIndex(int candidateMapIndex) {
     int[] pos = new int[2];
-    pos[0] = candidateMapIndex / boardWidth;
-    pos[1] = candidateMapIndex % boardWidth;
+    pos[0] = candidateMapIndex / 9;
+    pos[1] = candidateMapIndex % 9;
     return pos;
   }
 
-  private int calculateCandidateMapIndex(int row, int col) {
-    return row * boardWidth + col;
+  static List<Integer> calculateNextPos(int row, int col) {
+    List<Integer> nextPos = new ArrayList<>(Arrays.asList(row, col));
+    nextPos.set(1, col + 1);
+    if (nextPos.get(1) >= 9) {
+      if (row + 1 >= 9) {
+        return null;
+      }
+      nextPos.set(0, row + 1);
+      nextPos.set(1, 0);
+    }
+    return nextPos;
   }
 
-  private int[] calculateBlockFirstPos(int row, int col) {
+  static List<Integer> getNextEmptyPos(int row, int col, List<List<Integer>> puzzle) {
+    List<Integer> next = new ArrayList<>();
+    while (row < 9 && col < 9) {
+      next = calculateNextPos(row, col);
+      if (next == null) {
+        return null;
+      }
+      row = next.get(0);
+      col = next.get(1);
+      if (puzzle.get(row).get(col) == 0) {
+        break;
+      }
+    }
+    return next;
+  }
+
+  private static int calculateCandidateMapIndex(int row, int col) {
+    return row * 9 + col;
+  }
+
+  private static int[] calculateBlockFirstPos(int row, int col) {
     int[] topLeft = new int[2];
     topLeft[0] = (row / 3) * 3;
     topLeft[1] = (col / 3) * 3;
     return topLeft;
   }
 
-  private List<Integer> getBlockValues(int row, int col) {
+  private static List<Integer> getBlockValues(int row, int col, List<List<Integer>> puzzle) {
     int[] topLeft = calculateBlockFirstPos(row, col);
     List<Integer> blockValues = new ArrayList<>();
     for (int rowIndex = topLeft[0]; rowIndex < topLeft[0] + 3; rowIndex++) {
       for (int colIndex = topLeft[1]; colIndex < topLeft[1] + 3; colIndex++) {
-        blockValues.add(sudokuBoard.get(rowIndex).get(colIndex));
+        blockValues.add(puzzle.get(rowIndex).get(colIndex));
       }
     }
     return blockValues;
+  }
+
+  private List<Integer> getBlockValues(int row, int col) {
+    return getBlockValues(row, col, sudokuBoard);
   }
 
   private void refreshCandidateMap() {
@@ -303,5 +526,4 @@ public class SudokuSolver {
       }
     }
   }
-
 }
--- java-implementation/src/test/java/codingame/medium/SudokuSolverUt.java
+++ java-implementation/src/test/java/codingame/medium/SudokuSolverUt.java
@@ -6,47 +6,112 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static codingame.medium.SudokuSolver.calculateNextPos;
+import static codingame.medium.SudokuSolver.getAllBlockTopLeftPos;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SudokuSolverUt {
 
   private SudokuSolver testObject;
 
-  private SudokuSolverUt(){
+  private SudokuSolverUt() {
     String[] input = {
-            "800000000",
-            "003600000",
-            "070090200",
-            "050007000",
-            "000045700",
-            "000100030",
-            "001000068",
-            "008500010",
-            "090000400",
+      "800000000",
+      "003600000",
+      "070090200",
+      "050007000",
+      "000045700",
+      "000100030",
+      "001000068",
+      "008500010",
+      "090000400",
     };
-    List<List<Integer>> puzzle = new ArrayList<>();
-    for (String lineStr: input){
+    testObject = new SudokuSolver(convertSudokuStr2List(input));
+  }
+
+  private List<List<Integer>> convertSudokuStr2List(String[] sudokuString) {
+    List<List<Integer>> puzzleList = new ArrayList<>();
+    for (String lineStr : sudokuString) {
       List<Integer> lineArr = new ArrayList<>();
       for (char c : lineStr.toCharArray()) {
         lineArr.add(Integer.parseInt(String.valueOf(c)));
       }
-      puzzle.add(lineArr);
+      puzzleList.add(lineArr);
     }
-    testObject = new SudokuSolver(puzzle);
+    return puzzleList;
   }
 
   @Test
-  void testGetBlockCandidateMapIndexes(){
+  void testGetBlockCandidateMapIndexes() {
     List<Integer> indexes = testObject.getBlockCandidateMapIndexes(3, 4);
     List<Integer> expect = Arrays.asList(30, 31, 32, 39, 40, 41, 48, 49, 50);
     assertEquals(indexes, expect);
   }
 
   @Test
-  void testGetColValues(){
+  void testGetColValues() {
     List<Integer> indexes = testObject.getColValues(3);
-    List<Integer> expect = Arrays.asList(0,6,0,0,0,1,0,5,0);
+    List<Integer> expect = Arrays.asList(0, 6, 0, 0, 0, 1, 0, 5, 0);
     assertEquals(expect, indexes);
   }
 
+  @Test
+  void testGetAllBlockTopLeftPos() {
+    List<List<Integer>> allBlockTopLeftPos = getAllBlockTopLeftPos();
+    List<List<Integer>> expect = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        expect.add(Arrays.asList(i * 3, j * 3));
+      }
+    }
+    System.out.println("Expected:" + expect.toString());
+    assertEquals(expect, allBlockTopLeftPos);
+  }
+
+  @Test
+  void testCalculateNextPos() {
+    List<List<Integer>> poss =
+        Arrays.asList(
+            Arrays.asList(0, 0), Arrays.asList(0, 8), Arrays.asList(7, 8), Arrays.asList(8, 8));
+    List<List<Integer>> expects =
+        Arrays.asList(Arrays.asList(0, 1), Arrays.asList(1, 0), Arrays.asList(8, 0), null);
+    for (int i = 0; i < poss.size(); i++) {
+      assertIterableEquals(
+          expects.get(i), calculateNextPos(poss.get(i).get(0), poss.get(i).get(1)));
+    }
+  }
+
+  @Test
+  void testIsFinalAnswerFalse() {
+    String[] input = {
+      "120070560",
+      "507932080",
+      "000001000",
+      "010240050",
+      "308000402",
+      "070085010",
+      "000700000",
+      "080423701",
+      "034010028",
+    };
+    boolean expected = false;
+    assertEquals(expected, testObject.isFinalAnswer(convertSudokuStr2List(input)));
+  }
+
+  @Test
+  void testIsFinalAnswerTrue() {
+    String[] input = {
+      "123874569",
+      "567932184",
+      "849651237",
+      "916247853",
+      "358196472",
+      "472385916",
+      "291768345",
+      "685423791",
+      "734519628",
+    };
+    boolean expected = true;
+    assertEquals(expected, testObject.isFinalAnswer(convertSudokuStr2List(input)));
+  }
 }
