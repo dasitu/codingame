048529f09b6a0e20e5cb55b96b3c46d39b5769f8
dasitu <dasitu_h@163.com>
2019-08-30 14:13:24 +0800

    complete 100% cases with different idea

--- python-implementation/codingame/medium_regular_polygons.py
+++ python-implementation/codingame/medium_regular_polygons.py
@@ -1,4 +1,34 @@
 import math
+from itertools import combinations
+
+
+def count_of_regular_polygons(n):
+    result = 1
+    fermat_primes = [3, 5, 17, 257, 65537]
+
+    # formulate all combinations of fermat_primes
+    combination_values = fermat_primes[:]
+    for i in range(2, len(fermat_primes)+1):
+        all_combination = list(combinations(fermat_primes, i))
+        # print(all_combination)
+        for combination in all_combination:
+            value = 1
+            for v in combination:
+                value *= v
+            combination_values.append(value)
+    combination_values.sort()
+    # print(combination_values)
+
+    # for every possible values, check log2(n/f) and fermat itself(log2(n/f)==0)
+    for f in combination_values:
+        if f <= n:
+            result += math.floor(math.log2(n/f)) + 1  # additional 1 due to fermat itself, or log2(n/f) == 0
+        else:
+            break
+
+    # for log2(n) itself
+    result += math.floor(math.log2(n))
+    return result
 
 
 def can_be_draw_using_geometric_construction(n):
@@ -12,9 +42,7 @@ def can_be_draw_using_geometric_construction(n):
 
 
 a, b = [int(i) for i in input().split()]
-count = 0
-for i in range(a, b + 1):
-    if can_be_draw_using_geometric_construction(i):
-        count += 1
-
+count = count_of_regular_polygons(b) - count_of_regular_polygons(a)
+if can_be_draw_using_geometric_construction(a):
+    count += 1
 print(count)
